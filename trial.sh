f8a21c7aed110a282df2e61291d265bf0cfb85e7
xebitstudios <xebitstudios@yahoo.com>
2021-07-11 23:16:27 -0400

    Update trial.sh
    here

--- trial.sh
+++ trial.sh
@@ -1,79 +1,177 @@
-# echo creating angular app $1...
-echo creating angular app $(jq '.app.name' input.json)
-# ng new $1 --routing
-ng new $(jq '.app.name' input.json) --routing
-# echo changing into package $1...
-
-echo changing into package $(jq '.app.name' input.json)
-# cd $1
-cd $(jq '.app.name' input.json)
-
-echo generate components for the number of pages needed in the app
-for i in $(jq '.app.routes[] | .endpoint' input.json)
-do
-	# echo $i --- "create"
-	echo ${i#?}
-	ng generate component ${i#?}
-done
-# Add the new files and routes needed
+appname=$(jq '.app.name' input.json | cut -d '"' -f 2)
+appport=$(jq '.app.port' input.json | cut -d '"' -f 2)
+routeslist=$(jq '.app.routes[].route' input.json | cut -d '"' -f 2)
+svcslist=$(jq '.app.services[].name' input.json | cut -d '"' -f 2)
 def0=$(jq '.app.default_route' input.json | cut -d '"' -f 2)
+echo - Creating angular app $appname
+ng new $appname --routing
+echo - Changing into package $appname folder
 def1="{ path: '', redirectTo: '/$def0', pathMatch: 'full' }"
-# echo $def1
+echo $def1
+cd $appname
+echo - Generating components for the number of pages needed in the app
+for i in $routeslist
+do
+	echo ${i}
+	ng generate component ${i}
+done
+svcs="services/"
+cd src
+cd app
+mkdir $svcs
+cd $svcs
+echo - Generating services needed in the app
+for i in $svcslist
+do
+	echo ${i}
+	ng generate service ${i}
+done
+cd ..
+cd ..
+cd ..
+# Add the new files and routes needed
 strt0="Routes = ["
 end0="]"
 mid0=""
-cmm0=","
-# str0="$strt0$VAR0$end0"
-# echo "$VAR3"
+cmm0=", "
+echo - Getting path
+ls
+echo ....................
+# Add Bootstrap settings to angular.json file
+angjsonfile="angular.json"
+angjsonvalue=`cat $angjsonfile`
+echo $angjsonvalue
+echo ----------------------------
+a1='.projects.'
+a2='.architect.build.options.styles[0]'
+setgs=$(jq $a1$appname$a2 angular.json | cut -d '"' -f 2)
+# echo $setgs
+echo ----------------------------
+newsetgs='src/styles.css",
+    "node_modules/bootstrap/dist/css/bootstrap.min.css'
+newsetgsfile=${angjsonvalue/$setgs/$newsetgs}
+# echo $newsetgsfile
+echo ----------------------------
+echo $newsetgsfile>angular.json
+echo ....................
+routfile="app-routing.module.ts"
 comp="Component"
-value=`cat accounts/src/app/app-routing.module.ts`
-for i in $(jq '.app.routes[]' input.json | cut -d '"' -f 2)
+cd src
+# Add Bootstrap to index.html file
+indexfile="index.html"
+indexvalue=`cat $indexfile`
+orig="<app-root></app-root>"
+replce="<app-root></app-root>
+	<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>"
+newindexfile=${indexvalue/$orig/$replce}
+ls
+echo ....................
+echo $newindexfile>index.html
+# Add global styling to styles.css file
+stylesfile="styles.css"
+stylesvalue=`cat $stylesfile`
+sorig="/* You can add global styles to this file, and also import other style files */"
+sreplce="body { padding: 15px; }"
+newstylesfile=${stylesvalue/$sorig/$sreplce}
+ls
+echo ....................
+echo $newstylesfile>styles.css
+# Add the routes to the routing.ts file
+cd app
+value=`cat $routfile`
+for i in $routeslist
 do
-	echo $i #--- "create"
+	echo $i
 	foo=$i
-	# echo ${i} | cut -d '"' -f 2
 	tring0="{ path: 'dashboard', component: DashboardComponent }"
-	# echo "${tring0/dashboard/$i}"
 	tring1=${tring0/dashboard/$i}
-	# echo $tring1
 	repl0="$(tr '[:lower:]' '[:upper:]' <<< ${foo:0:1})${foo:1}"
-	# echo $repl0
-	# echo "${tring1/Dashboard/$repl0}"
 	mid1="${tring1/Dashboard/$repl0}"
-	# echo $strt0$mid0$end0
 	mid0=$mid0$cmm0$mid1
-	# echo $tring
-	ng generate component ${i} | cut -d '"' -f 2
 	# add the import lines for the components
 	oldConst="const"
 	newConst="import { $repl0$comp } from './$i/$i.component'; const"
-	# echo $newConst
-	value="${value//$oldConst/$newConst}"
-	
+	echo - NewConst....
+	echo $newConst
+	echo ....................
+	value="${value/$oldConst/$newConst}"	
 done
-
 echo 
-# value=`cat routing.ts`
-# cd "accounts/src/app/"
-# value=`cat accounts/src/app/app-routing.module.ts`
-# echo "$value"
-# echo "---"
 oldString="Routes = []"
 newString="Routes = [
 	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent }
 ]"
-echo "${value//$oldString/$newString}" #> ra.ts
-rm accounts/src/app/app-routing.module.ts
-echo 
-"${value//$oldString/$strt0$def1$mid0$end0}" > accounts/src/app/app-routing.module.ts
-echo "done..."
+varr=${value/$oldString/$newString}
+ls
+echo ....................
+barr=${value/$oldString/$strt0$def1$mid0$end0}
+echo $barr>app-routing.module.ts
+# Add httpClient and RxJS to services
+cd services
+svcts=".service.ts"
+# const createURL = 'http://localhost:8100/api/consumer';
+# const findAllURL = 'http://localhost:8100/api/consumer/findall';
+# Create the service API endpoints to add to service files
+
+echo - Configuring service files
+for i in $svcslist
+do
+	svcfile=`cat $i$svcts`
+	oldsvc="import { Injectable } from '@angular/core';"
+	newsvc="import { Injectable } from '@angular/core';
+		import { HttpClient } from '@angular/common/http';
+		import { Observable } from 'rxjs';
+		import { analyzeAndValidateNgModules } from '@angular/compiler';"
+	varsvc=${svcfile/$oldsvc/$newsvc}
+	oldsvc="constructor() { }"
+	newsvc="constructor(private httpClient: HttpClient) { }"
+	thesvc=${varsvc/$oldsvc/$newsvc}
+	echo $thesvc>$i$svcts
+done
+cd ..
+# Add a Navbar to landing page
+echo - Navigate to landing page...
+cd ..
+ls
+echo ....................1
+cd $def0
+ls
+echo ....................2
+navbr1="<nav class='navbar navbar-expand-lg navbar-light bg-light'>
+  <a class='navbar-brand' href='/'>$(tr '[:lower:]' '[:upper:]' <<< ${appname:0:1})${appname:1}</a>
+  <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNavAltMarkup' aria-controls='navbarNavAltMarkup' aria-expanded='false' aria-label='Toggle navigation'>
+    <span class='navbar-toggler-icon'></span>
+  </button>
+  <div class='collapse navbar-collapse' id='navbarNavAltMarkup'>
+    <div class='navbar-nav'>"
+navbr2="</div>
+  </div>
+</nav>
+<router-outlet></router-outlet>"
+for i in $routeslist
+do
+	echo - Adding the Navbar Links...
+	lnk="<a class='nav-item nav-link' href='/$i'>$(tr '[:lower:]' '[:upper:]' <<< ${i:0:1})${i:1}</a>"
+	navbr1=$navbr1$lnk
+done
+echo - Navbr1...
+echo $navbr1
+echo .....
+nvbr=$navbr1$navbr2
+echo $nvbr
+echo .....+.....
+ls
+cd app
+echo $nvbr>app.component.html
+echo .....+.....
+echo "- Done..."
 # Add Bootstrap and install all dependencies
-echo installing bootstrap...
+echo - Installing bootstrap...
 npm install bootstrap
 
-echo running npm install...
+echo - Running npm install...
 npm install
 
-echo starting $(jq '.app.name' input.json) app...
-ng serve --port 4201
+echo - Starting $appname app...
+ng serve --port $appport
